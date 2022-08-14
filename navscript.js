bda9b38d81cd32c823f51335f6c959b9df7e628b
conniexhe <83856884+conniexhe@users.noreply.github.com>
2022-08-13 22:42:29 -0700

    Add files via upload

---
+++ navscript.js
@@ -0,0 +1,17 @@
+// function myFunction() {
+//   var x = document.getElementById("navBar");
+//
+//   if (x.className === ".topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = ".topnav";
+//   }
+// }
+
+function openNav() {
+  document.getElementById("pages").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("pages").style.height = "0%";
+}
